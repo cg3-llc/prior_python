5bc4259335f493760edaa003ec6551eacc8850f4
Charlie Mulic <charles.mulic@gmail.com>
2026-02-18 11:11:46 -0600

    bump v0.1.3: trigger-based instructions

--- src/prior_tools/__init__.py
+++ src/prior_tools/__init__.py
@@ -4,7 +4,7 @@ from .tools import PriorSearchTool, PriorContributeTool, PriorFeedbackTool, Prio
 from .client import PriorClient
 from .config import load_config, save_config
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = [
     "PriorSearchTool",
     "PriorContributeTool",
--- src/prior_tools/client.py
+++ src/prior_tools/client.py
@@ -7,7 +7,7 @@ import requests
 
 from .config import load_config, save_config
 
-USER_AGENT = "prior-python/0.1.2"
+USER_AGENT = "prior-python/0.1.3"
 
 
 class PriorClient:
